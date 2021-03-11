0d997436aba9364601b178a7e111faa6204ea824
Kosiarz101 <pol1100@o2.pl>
2021-03-11 18:46:28 +0100

    Project AddressBookPS5 and .gitignore file have been added

---
+++ AddressBookPS5/Forms/Address.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBookPS5.Forms
+{
+    public class Address
+    {
+        [Display(Name = "Twoja Ulubiona Ulica")]
+        [StringLength(60, MinimumLength = 5, ErrorMessage = "Pole {0} musi zawierać conajmniej {2} znaków oraz nie więcej niż {1} znaków"), Required(ErrorMessage ="Pole jest obowiązkowe")]
+        public string Street { get; set; }
+        [Required(ErrorMessage = "Pole jest obowiązkowe")]
+        public int Number { get; set; }
+        [Required(ErrorMessage = "Pole jest obowiązkowe")]
+        public string ZipCode { get; set; }
+        [Required(ErrorMessage = "Pole jest obowiązkowe")]
+        public string City { get; set; }
+    }
+}
