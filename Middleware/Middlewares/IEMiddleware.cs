59f729c61b6ebd0945999cc80b2c3e36dddd34c6
Kosiarz101 <pol1100@o2.pl>
2021-04-15 18:50:34 +0200

    Add Project Middleware

---
+++ Middleware/Middlewares/IEMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wangkanai.Detection.Models;
+using Wangkanai.Detection.Services;
+
+namespace Middleware.Middlewares
+{
+    public class IEMiddleware
+    {
+        private RequestDelegate _next;
+        private StringValues userAgent;
+
+        public IEMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public Task Invoke(HttpContext context, IDetectionService detection)
+        {
+            if (detection.Browser.Name == Browser.InternetExplorer || detection.Browser.Name == Browser.Edge)
+            {
+                context.Response.ContentType = "text/plain; charset=utf-8";
+                context.Response.WriteAsync("Przeglądarka nie jest obsługiwana");
+                return Task.Run(() => { });
+            }              
+            if(context.Request.Headers.TryGetValue("User-Agent", out userAgent))
+            {
+                context.Response.WriteAsync(userAgent);
+            }           
+            return _next(context);
+        }
+    }
+}
---
+++ Middleware/Middlewares/IEMiddlewareExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Middleware.Middlewares
+{
+    public static class IEMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseIEMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<IEMiddleware>();
+        }
+    }
+}
