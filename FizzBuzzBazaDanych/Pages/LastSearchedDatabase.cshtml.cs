f8d29773b72468f0f8bf042a846ed1420ddb13b4
Kosiarz101 <pol1100@o2.pl>
2021-04-29 17:26:39 +0200

    Add entries filter

--- FizzBuzzBazaDanych/Pages/LastSearchedDatabase.cshtml.cs
+++ FizzBuzzBazaDanych/Pages/LastSearchedDatabase.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using FizzBuzzBazaDanych.Data;
 using FizzBuzzBazaDanych.Forms;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -14,10 +15,12 @@ namespace FizzBuzzBazaDanych.Pages
     public class LastSearchedDatabaseModel : PageModel
     {
         private readonly FizzBuzzContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
         public IList<FizzBuzz> FizzBuzzEntries { get; set; }
-        public LastSearchedDatabaseModel(FizzBuzzContext context)
+        public LastSearchedDatabaseModel(FizzBuzzContext context, UserManager<IdentityUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
         public void OnGet()
         {
@@ -25,6 +28,15 @@ namespace FizzBuzzBazaDanych.Pages
                                 orderby FizzBuzzData.LastSearchDate descending
                                 select FizzBuzzData).Take(20);
             FizzBuzzEntries = FizzBuzzQuery.ToList();
-        }      
+        }
+        public void OnGetUserList()
+        {
+            string userId = _userManager.GetUserId(User);
+            var FizzBuzzQuery = (from FizzBuzzData in _context.FizzBuzzData
+                                 where FizzBuzzData.Creator == userId
+                                 orderby FizzBuzzData.LastSearchDate descending
+                                 select FizzBuzzData).Take(20);
+            FizzBuzzEntries = FizzBuzzQuery.ToList();
+        }
     }
 }
